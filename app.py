dfad9812288c97d746ae6eabe8f51c85108e9187
Cj <cjvillafuerte02@gmaik.com>
2022-09-19 00:51:41 +0800

    test api

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from ml_news_core import similarNews
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Machine Learning API for Balanced News Summary" # Just updating my code
+
+@app.route("/ml-related-news",methods=["GET"])
+def api():
+    args = request.args
+    return args
---
+++ test.py
@@ -0,0 +1,3 @@
+from ml_news_core import similarNews
+
+print(similarNews("https://edition.cnn.com/2022/09/17/asia/taiwan-drones-china-gray-zone-warfare-intl-hnk-dst/index.html"))
